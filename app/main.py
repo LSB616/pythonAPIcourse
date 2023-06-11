4847707649e9d533e3c1fbcfb1258d89ad5df420
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-06-12 00:56:11 +0100

    created users table, implementing password hashing, and introduced routers

--- app/main.py
+++ app/main.py
@@ -1,26 +1,17 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-from typing import Optional
+from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
-from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from . import models
-from .database import engine, get_db
-from sqlalchemy.orm import Session
+from .database import engine
+from .routers import post, user
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 load_dotenv()
-
-class Post(BaseModel):
-    title:  str 
-    content: str
-    published: bool = True
 
 while True:
     try:
@@ -33,51 +24,11 @@ while True:
         print("Error: ", error)
         time.sleep(2)
 
+app.include_router(post.router)
+app.include_router(user.router)
+
+
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to my FASTAPI"}
-
-@app.get("/posts")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {"data": posts}
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def create_posts(post: Post, db: Session = Depends(get_db)):
-    new_post = models.Post(**post.dict())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return {"data": new_post}
-
-@app.get("/posts/{id}")
-def get_post(id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post With ID: {id} Was Not Found")
-    return {"data": post}
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(id: int, db: Session = Depends(get_db)):
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-
-    if post_query.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post With ID: {id} Does Not Exist")
-    
-    post_query.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
-
-    if post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post With ID: {id} Does Not Exist")
-    
-    post_query.update(updated_post.dict(), synchronize_session=False)
-    db.commit()
-    return {"data": post_query.first()}
-
+    return {"message": "Welcome To My FASTAPI"}
